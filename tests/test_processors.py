18c451f735dda81ef0455258f394fa0098ee79b1
Alexander Kazakov <kazakov.a.s.90@gmail.com>
2017-04-01 20:34:06 +0100

    Add test find for processors

---
+++ tests/test_processors.py
@@ -0,0 +1,23 @@
+from __future__ import print_function
+from nifi import Nifi
+import requests
+
+
+processor_id = {}
+nifi = {}
+resource = {}
+
+
+def setup_module(module):
+    resp = requests.get('http://localhost:8080/nifi-api/flow/search-results?q=')
+    global processor_id, nifi, resource
+    processor_id = resp.json()['searchResultsDTO']['processorResults'][0]['id']
+    nifi = Nifi('http://localhost:8080/nifi-api')
+    resource = nifi.resource('processors')
+
+
+def test_find():
+    processor = resource.find(processor_id)
+    assert 'id' in processor
+    assert 'component' in processor
+    assert 'revision' in processor
